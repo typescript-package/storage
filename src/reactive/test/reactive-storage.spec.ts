03ca63d6a98964be8f3a0f5a46726e8a5170800a
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2026-02-15 18:36:41 +0000

    fix(storage): enable emitter initialization in ReactiveStorage test

--- src/reactive/test/reactive-storage.spec.ts
+++ src/reactive/test/reactive-storage.spec.ts
@@ -1,5 +1,4 @@
 
-
 import { NamedEventEmitter } from "@typescript-package/event-emitter";
 import { WebStorageAdapter } from "../../adapter";
 import { ReactiveStorage } from "../src/lib";
@@ -8,11 +7,15 @@ const storage = new ReactiveStorage(
   {async: false, value: {a: 1, b: 2}},
   WebStorageAdapter,
   localStorage,
-  'test-storage',
+  'test-storage', 
   JSON.stringify,
   JSON.parse
 );
 
-// storage.setEmitter(NamedEventEmitter);
+storage.setEmitter(NamedEventEmitter);
 
+storage.on('put', ({key, value}) => {
+  console.log(`put event: key=${key}, value=${value}`);
+});
 
+storage.put('b', 3);
